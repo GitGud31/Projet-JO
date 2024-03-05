d364c0f2f1b444dabb17cd45d82833998aadb296
GitGud31 <mhamedlehbab@gmail.com>
2024-03-05 12:26:43 +0100

    added url strategy for web

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:front/root.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
+
   runApp(
     const ProviderScope(child: Root()),
   );
