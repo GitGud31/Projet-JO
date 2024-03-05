125b11793e1c7b6b1e38e80639450bef8ab71e52
GitGud31 <mhamedlehbab@gmail.com>
2024-03-05 15:25:29 +0100

    finished LandingScreen

---
+++ front/lib/conts/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const white = Colors.white;
+final gold = Colors.yellow[800];
---
+++ front/lib/conts/styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final titleStyle = GoogleFonts.rubik(
+  fontSize: 40,
+  fontWeight: FontWeight.bold,
+);
+
+final buttonTextStyle = GoogleFonts.poppins(
+  color: Colors.white,
+  fontSize: 15,
+);
