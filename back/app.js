cbc2310ffb0182af948194c401440edf20704e9b
GitGud31 <mhamedlehbab@gmail.com>
2024-03-07 11:42:55 +0100

    added cors

--- back/app.js
+++ back/app.js
@@ -1,27 +1,28 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import athleteRoute from './routes/athleteRoutes.js';
-import epreuveRoute from './routes/epreuveRoutes.js';
-import paysRoute from './routes/paysRoutes.js'
-import sportRoute from './routes/sportRoutes.js'
-
+import express from "express";
+import bodyParser from "body-parser";
+import athleteRoute from "./routes/athleteRoutes.js";
+import epreuveRoute from "./routes/epreuveRoutes.js";
+import paysRoute from "./routes/paysRoutes.js";
+import sportRoute from "./routes/sportRoutes.js";
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 // Middleware
 app.use(bodyParser.json());
 
 // Routes
-app.use('/api', athleteRoute);
-app.use('/api', epreuveRoute);
-app.use('/api',paysRoute);
-app.use('/api',sportRoute);
-
+app.use("/api", athleteRoute);
+app.use("/api", epreuveRoute);
+app.use("/api", paysRoute);
+app.use("/api", sportRoute);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
 });
 
 // Start the server
