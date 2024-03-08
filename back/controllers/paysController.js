43b86d364461e814f18eb5d9e152dcdbdafef666
oualid rahhouti <rahhoutiwalid@gmail.com>
2024-03-08 12:31:23 +0100

    delete according athletes and epreuves when deleting pays

--- back/controllers/paysController.js
+++ back/controllers/paysController.js
@@ -57,15 +57,35 @@ router.put('/:id', async (req, res) => {
 
 // Delete a country
 router.delete('/:id', async (req, res) => {
+  const transaction = await models.sequelize.transaction(); // Start a transaction
+
   try {
-    const country = await models.PaysModel.findByPk(req.params.id);
-    if (!country) {
-      res.status(404).json({ message: 'Country not found' });
-    } else {
-      await country.destroy();
-      res.status(204).end();
-    }
+    const paysId = req.params.id;
+    
+    // Find all associated paysEpreuve records
+    const paysEpreuveRecords = await models.PaysEpreuveModel.findAll({ where: { paysId: paysId } });
+    console.log(paysEpreuveRecords)
+    // Collect epreuve IDs from paysEpreuve records
+    const epreuveIds = paysEpreuveRecords.map(record => record.epreuveId);
+    console.log(epreuveIds)
+
+    // Delete associated epreuve records
+    await models.EpreuveModel.destroy({ where: { id: epreuveIds }, transaction });
+
+    // Delete associated paysEpreuve records
+    await models.PaysEpreuveModel.destroy({ where: { paysId: paysId }, transaction });
+
+    // Delete related athletes
+    await models.AthleteModel.destroy({ where: { paysId: paysId }, transaction });
+
+
+    // Delete the country
+    await models.PaysModel.destroy({ where: { id: paysId }, transaction });
+
+    await transaction.commit(); // Commit transaction
+    res.status(204).end();
   } catch (error) {
+    await transaction.rollback(); // Rollback transaction if any error occurs
     res.status(500).json({ error: error.message });
   }
 });
